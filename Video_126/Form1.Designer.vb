0ee0960ca1d3719d7dc6aac49c46a58056dc48cf
Nikoloz Chapichadze <nikoloz.chapichadze@gmail.com>
2022-02-05 12:54:28 -0500

    Finished 127. Isnt able to write in document

--- Video_126/Form1.Designer.vb
+++ Video_126/Form1.Designer.vb
@@ -22,17 +22,53 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.btnOpenFile = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.btnWriteFile = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'btnOpenFile
+        '
+        Me.btnOpenFile.Location = New System.Drawing.Point(12, 208)
+        Me.btnOpenFile.Name = "btnOpenFile"
+        Me.btnOpenFile.Size = New System.Drawing.Size(125, 23)
+        Me.btnOpenFile.TabIndex = 0
+        Me.btnOpenFile.Text = "Open File"
+        Me.btnOpenFile.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(6, 12)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(294, 190)
+        Me.TextBox1.TabIndex = 1
+        '
+        'btnWriteFile
+        '
+        Me.btnWriteFile.Location = New System.Drawing.Point(176, 208)
+        Me.btnWriteFile.Name = "btnWriteFile"
+        Me.btnWriteFile.Size = New System.Drawing.Size(117, 23)
+        Me.btnWriteFile.TabIndex = 2
+        Me.btnWriteFile.Text = "Write File"
+        Me.btnWriteFile.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(396, 243)
+        Me.ClientSize = New System.Drawing.Size(305, 243)
+        Me.Controls.Add(Me.btnWriteFile)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btnOpenFile)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents btnOpenFile As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents btnWriteFile As Button
 End Class
--- Video_126/Form1.vb
+++ Video_126/Form1.vb
@@ -1,3 +1,22 @@
-﻿Public Class Form1
+﻿Imports System.IO
+Imports System.Text
 
+Public Class Form1
+
+    Private fs As FileStream
+    Private path As String = My.Computer.FileSystem.SpecialDirectories.MyDocuments + "\Hello.txt"
+
+    Private Sub btnOpenFile_Click(sender As Object, e As EventArgs) Handles btnOpenFile.Click
+        fs = File.OpenWrite(path)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox1.Text = File.ReadAllText(path)
+        File.SetAttributes(path, FileAttributes.Hidden)
+    End Sub
+
+    Private Sub btnWriteFile_Click(sender As Object, e As EventArgs) Handles btnWriteFile.Click
+        Dim fileWords As Byte() = New UTF8Encoding(True).GetBytes(TextBox1.Text)
+        fs.Write(fileWords, 0, fileWords.Length)
+    End Sub
 End Class
