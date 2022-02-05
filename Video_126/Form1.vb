46a64a6510c367e051e9fbd39b57b545cd3a7799
Mark Lounsbury <mark@gothinkabout.com>
2022-02-05 15:09:21 -0500

    if you haven't clicked the btnOpenFile, the application doiesn't know what file to use yet. to add in some fault tolerance, open the file before trying to use it.

--- Video_126/Form1.vb
+++ Video_126/Form1.vb
@@ -16,7 +16,15 @@ Public Class Form1
     End Sub
 
     Private Sub btnWriteFile_Click(sender As Object, e As EventArgs) Handles btnWriteFile.Click
+        'if you haven't clicked the btnOpenFile, the application doiesn't know what file to use yet.
+        'to add in some fault tolerance, open the file before trying to use it.
+        Dim fsWrite As FileStream = File.OpenWrite(path)
+
         Dim fileWords As Byte() = New UTF8Encoding(True).GetBytes(TextBox1.Text)
-        fs.Write(fileWords, 0, fileWords.Length)
+
+        fsWrite.Write(fileWords, 0, fileWords.Length)
+
+        'cloce the file when you're done.
+        fsWrite.Close()
     End Sub
 End Class
